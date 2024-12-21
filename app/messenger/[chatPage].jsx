c7e5f855a9a740096e04f5e514fccf6878864d2a
raja-0007 <128035575+raja-0007@users.noreply.github.com>
2024-12-21 15:42:22 +0530

    chat funcitionality updated with sockets

--- app/messenger/[chatPage].jsx
+++ app/messenger/[chatPage].jsx
@@ -6,138 +6,156 @@ import ChatHeader from '@/components/messenger components/ChatHeader'
 import ChatFooter from '@/components/messenger components/ChatFooter'
 import Chatspace from '@/components/messenger components/Chatspace'
 import io from 'socket.io-client'; // Import socket.io-client
+import axios from 'axios'
 
 const chatPage = () => {
-    const { chatPage } = useLocalSearchParams()
-    const [ws, setWs] = useState(null);
-    const [isSocketDisconnected, setIsSocketDisconnected] = useState(true)
-    const [messages, setMessages] = useState([])
-    useEffect(() => {
-        if (!ws) {
-            const socket = io(process.env.EXPO_PUBLIC_BASE_URL, {
-                transports: ['websocket'],
-                forceNew: true,
-                reconnectionAttempts: 5,
-                timeout: 10000,
-            });
-    
-            setWs(socket);
-    
-            socket.on('connect', () => console.log('WebSocket connected'));
-            socket.on('disconnect', () => console.log('Disconnected from server'));
-            socket.on('message', (data) =>{
-                 console.log('Message from server:', data)
-                 setMessages([
-                    {
-                      "from": "raja",
-                      "message": "Hi, did you get a chance to review the document I sent?",
-                      "sent_at": "9:15am",
-                      "date": "2024-12-11"
-                    },
-                    {
-                      "from": "user1",
-                      "message": "Yes, I did. It looks great, but I have a few suggestions.",
-                      "sent_at": "9:20am",
-                      "date": "2024-12-11"
-                    },
-                    {
-                      "from": "raja",
-                      "message": "Perfect. Let me know when you have time to discuss them.",
-                      "sent_at": "9:22am",
-                      "date": "2024-12-11"
-                    },
-                    {
-                      "from": "user1",
-                      "message": "We can connect after lunch, say 2 PM?",
-                      "sent_at": "9:25am",
-                      "date": "2024-12-11"
-                    },
-                    {
-                      "from": "raja",
-                      "message": "Sounds good. See you then!",
-                      "sent_at": "9:27am",
-                      "date": "2024-12-11"
-                    },
-                    {
-                      "from": "raja",
-                      "message": "I was thinking we could catch up over coffee. It’s been a while.",
-                      "sent_at": "10:20am",
-                      "date": "2024-12-12"
-                    },
-                    {
-                      "from": "user1",
-                      "message": "That sounds perfect! Let’s do it.",
-                      "sent_at": "10:22am",
-                      "date": "2024-12-12"
-                    },
-                    {
-                      "from": "raja",
-                      "message": "Great! How about Saturday at 3 PM?",
-                      "sent_at": "10:23am",
-                      "date": "2024-12-12"
-                    },
-                    {
-                      "from": "user1",
-                      "message": "Works for me. Do you have a place in mind?",
-                      "sent_at": "10:24am",
-                      "date": "2024-12-12"
-                    },
-                    {
-                      "from": "raja",
-                      "message": "There’s a new cafe downtown called 'Brew Haven'. Heard good things about it.",
-                      "sent_at": "10:26am",
-                      "date": "2024-12-12"
-                    },
-                    {
-                      "from": "user1",
-                      "message": "Perfect! Let’s meet there. Should I make a reservation?",
-                      "sent_at": "10:28am",
-                      "date": "2024-12-12"
-                    },
-                    {
-                      "from": "raja",
-                      "message": "That would be great, thanks! Let’s plan to meet at the entrance.",
-                      "sent_at": "10:30am",
-                      "date": "2024-12-12"
-                    },
-                    {
-                      "from": "user1",
-                      "message": "Got it. Looking forward to catching up!",
-                      "sent_at": "10:31am",
-                      "date": "2024-12-12"
-                    },
-                    {
-                      "from": "raja",
-                      "message": "Me too! See you on Saturday.",
-                      "sent_at": "10:32am",
-                      "date": "2024-12-12"
-                    }
-                  ]
-                  )
-                });
-            socket.on('connect_error', (err) => console.error('Connection error:', err));
-    
-            return () => socket.disconnect(); // Cleanup
+  const currentUser = 'raja'
+  const { chatPage } = useLocalSearchParams()
+  const [ws, setWs] = useState(null);
+  const [isSocketDisconnected, setIsSocketDisconnected] = useState(true)
+  const [messages, setMessages] = useState([])
+  const [socketID, setSocketID] = useState(null)
+  useEffect(() => {
+    if (!ws) {
+      const socket = io(process.env.EXPO_PUBLIC_BASE_URL, {
+        transports: ['websocket'],
+        forceNew: true,
+        reconnectionAttempts: 5,
+        timeout: 10000,
+      });
+
+      setWs(socket);
+
+      socket.on('connect', () => console.log('WebSocket connected'));
+      socket.on('yourSocketId', (socketId) => {
+        console.log('My socketId:', socketId);
+        setSocketID(socketId);
+        // You can now store this socketId on the client
+      });
+      socket.on('disconnect', () => console.log('Disconnected from server'));
+      socket.on('message', (data) => {
+        console.log('Message from server:', data)
+        if (data.status == 'message saved') {
+          setMessages(data.newMessages)
         }
-    }, []);
+        else {
+          setMessages([
+            {
+              "from": "raja",
+              "message": "Hi, did you get a chance to review the document I sent?",
+              "sent_at": "9:15am",
+              "date": "2024-12-11"
+            },
+            {
+              "from": "user1",
+              "message": "Yes, I did. It looks great, but I have a few suggestions.",
+              "sent_at": "9:20am",
+              "date": "2024-12-11"
+            },
+            {
+              "from": "raja",
+              "message": "Perfect. Let me know when you have time to discuss them.",
+              "sent_at": "9:22am",
+              "date": "2024-12-11"
+            },
+            {
+              "from": "user1",
+              "message": "We can connect after lunch, say 2 PM?",
+              "sent_at": "9:25am",
+              "date": "2024-12-11"
+            },
+            {
+              "from": "raja",
+              "message": "Sounds good. See you then!",
+              "sent_at": "9:27am",
+              "date": "2024-12-11"
+            },
+            {
+              "from": "raja",
+              "message": "I was thinking we could catch up over coffee. It’s been a while.",
+              "sent_at": "10:20am",
+              "date": "2024-12-12"
+            },
+            {
+              "from": "user1",
+              "message": "That sounds perfect! Let’s do it.",
+              "sent_at": "10:22am",
+              "date": "2024-12-12"
+            },
+            {
+              "from": "raja",
+              "message": "Great! How about Saturday at 3 PM?",
+              "sent_at": "10:23am",
+              "date": "2024-12-12"
+            },
+            {
+              "from": "user1",
+              "message": "Works for me. Do you have a place in mind?",
+              "sent_at": "10:24am",
+              "date": "2024-12-12"
+            },
+            {
+              "from": "raja",
+              "message": "There’s a new cafe downtown called 'Brew Haven'. Heard good things about it.",
+              "sent_at": "10:26am",
+              "date": "2024-12-12"
+            },
+            {
+              "from": "user1",
+              "message": "Perfect! Let’s meet there. Should I make a reservation?",
+              "sent_at": "10:28am",
+              "date": "2024-12-12"
+            },
+            {
+              "from": "raja",
+              "message": "That would be great, thanks! Let’s plan to meet at the entrance.",
+              "sent_at": "10:30am",
+              "date": "2024-12-12"
+            },
+            {
+              "from": "user1",
+              "message": "Got it. Looking forward to catching up!",
+              "sent_at": "10:31am",
+              "date": "2024-12-12"
+            },
+            {
+              "from": "raja",
+              "message": "Me too! See you on Saturday.",
+              "sent_at": "10:32am",
+              "date": "2024-12-12"
+            }
+          ]
+          )
 
-    const sendMessage = () => {
-        if (ws) {
-            ws.emit('message', 'Hello from the client!');
-          }
-    };
+        }
+      });
+      socket.on('connect_error', (err) => console.error('Connection error:', err));
 
-    return (
-        <SafeAreaWrapper>
-            <View className='flex-1 relative'>
-                <ChatHeader title={chatPage} sendMessage={sendMessage} type={'chat'} />
-                <Chatspace messages={messages} />
-                <ChatFooter />
-            </View>
+      return () => socket.disconnect(); // Cleanup
+    }
+  }, []);
 
-        </SafeAreaWrapper>
+  const sendMessage = async(value) => {
+    // if (ws) {
+      // ws.emit('message', 'Hello from the client!');
+      // ws.emit('message', { from: currentUser, to: chatPage, message: value, socketId: socketID });
+      console.log('sending message')
+      await axios.post(`${process.env.EXPO_PUBLIC_BASE_URL}/newMessage`,{ from: currentUser, to: chatPage, message: value, socketId: socketID })
+      .then(res=>console.log('message sent'))
+    // }
+  };
 
-    )
+  return (
+    <SafeAreaWrapper>
+      <View className='flex-1 relative'>
+        <ChatHeader title={chatPage} sendMessage={sendMessage} type={'chat'} />
+        <Chatspace messages={messages} />
+        <ChatFooter sendMessage={sendMessage} />
+      </View>
+
+    </SafeAreaWrapper>
+
+  )
 }
 
 export default chatPage
--- components/messenger components/ChatFooter.jsx
+++ components/messenger components/ChatFooter.jsx
@@ -1,15 +1,28 @@
 import { View, Text, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-const ChatFooter = () => {
+const ChatFooter = ({sendMessage}) => {
+  const [message, setMessage] = useState('')
+  const changehandler = (e)=>{
+    // console.log(e.nativeEvent.text)
+    setMessage(e.nativeEvent.text)
+}
+
+  const submitHandler=()=>{
+    if(message !== ''){
+      console.log('messagemessagemessagemessage',message)
+      sendMessage(message)
+      setMessage('')
+    }
+  }
   return (
     <View className='px-8  h-[9vh] flex flex-row items-center justify-center gap-2'>
         <TextInput multiline returnKeyType='send'
         // value={comment} onChangeText={setComment}
-         placeholder={`message`} className='border rounded-full px-5 border-gray-300 w-[90%] h-12' />
+         placeholder={`message`} value={message} onChange={changehandler} className='border rounded-full px-5 border-gray-300 w-[90%] h-12' />
          <View className='h-12 w-12 rounded-full bg-orange-300 flex items-center justify-center'>
-          <MaterialCommunityIcons name="send" className='ms-1' size={20} color="black" 
+          <MaterialCommunityIcons name="send" className='ms-1' onPress={submitHandler} size={20} color="black" 
         // onPress={submitComment}
          /></View>
         
